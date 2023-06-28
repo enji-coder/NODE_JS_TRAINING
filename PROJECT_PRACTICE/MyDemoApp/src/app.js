94038df2fc052441e1a7b4ef1d733bbe151f3b34
enji-coder <enji.20.patel@gmail.com>
2023-06-28 12:30:21 +0530

    updates

--- PROJECT_PRACTICE/MyDemoApp/src/app.js
+++ PROJECT_PRACTICE/MyDemoApp/src/app.js
@@ -8,11 +8,16 @@ app.use("",routes)
 app.set("view engine","hbs")
 app.set("views","views")
 
-// configure public folder as a static 
+// configure public folder as a static
+// const path = require("path")
+// const public_path = path.join(__dirname,"./public") 
 app.use("/static",express.static("public"))
+//              
+//app.use(express.static(public_path))
 
 
 app.listen(process.env.PORT | 5556,()=> {
     console.log("server started")
+    
 })
 
