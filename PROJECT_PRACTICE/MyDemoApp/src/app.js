e71284d65d4d93b0ac477bc5b3d1d68c04a3738d
enji-coder <enji.20.patel@gmail.com>
2023-06-28 16:24:29 +0530

    Updates

--- PROJECT_PRACTICE/MyDemoApp/src/app.js
+++ PROJECT_PRACTICE/MyDemoApp/src/app.js
@@ -3,21 +3,35 @@ const express = require("express")
 const app = express() 
 const routes = require("./routes/main")
 
+// need to import mongoose for database connectivity
+const mongoose = require("mongoose")
+
 app.use("",routes)
 
 app.set("view engine","hbs")
 app.set("views","views")
 
+const hbs = require("hbs")
+hbs.registerPartials("views/partials")
+
+
 // configure public folder as a static
 // const path = require("path")
 // const public_path = path.join(__dirname,"./public") 
 app.use("/static",express.static("public"))
-//              
+
 //app.use(express.static(public_path))
+
+
+// ------------ DB CONNECTION -----------------
+// mongoose.connect("mongodb://localhost/website_demo")
+// ------------ DB CONNECTION -----------------
+
 
 
 app.listen(process.env.PORT | 5556,()=> {
     console.log("server started")
-    
 })
+
+
 
